b417dcbabce2fd97046a1bc3f56e76bccb9c0f12
txl302 <txl302@case.edu>
2020-04-24 01:18:38 -0400

    animation

---
+++ animation/animation.py
@@ -0,0 +1,22 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+def update_points(num):
+	point_ani.set_data(x[num], y[num])
+	return point_ani
+
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+
+fig = plt.figure(tight_layout = True)
+plt.plot(x,y)
+
+point_ani, = plt.plot(x[0], y[0], 'ro')
+plt.grid(ls = "--")
+
+ani = matplotlib.animation.FuncAnimation(fig, update_points, np.arange(0, 100), interval=100)
+
+plt.show()
+
